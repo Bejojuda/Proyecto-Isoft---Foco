c1620f896d9d07ebbc74f142ca094195ac4ec28f
Juan <juanma.712@hotmail.com>
2017-11-16 17:46:44 -0500

    Codigo 75% hecho

--- src/main.cpp
+++ src/main.cpp
@@ -9,35 +9,33 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 
-const char* ssid="TORRES PEREZ";
-const char* password="0008151964";
+const char* ssid="TORRES PEREZ";//Progsistemas
+const char* password="0008151964";//scndsemsept17
 char mqtt_server[]="m12.cloudmqtt.com";
-const int led=16;
-const int button=0;
-const int sensor=12;
+const int foco=16;
 int cronometro=0;
 String estado_foco;
 WiFiClient espClient;
-WiFiClient telnet;
 WiFiServer server(23);
 PubSubClient client(espClient);
 MDNSResponder mdns;
-
-String realSize = String(ESP.getFlashChipRealSize());
-String ideSize = String(ESP.getFlashChipSize());
-bool flashCorrectlyConfigured = realSize.equals(ideSize);
-
 bool shouldSaveConfig=false;
+
 
 
 void setup_wifi(){
   Serial.println();
   Serial.print("conectandose a ");
   Serial.println(ssid);
-  WiFi.begin(ssid,password);
-  while(WiFi.status()!=WL_CONNECTED){
-    delay(500);
-    Serial.print(".");
+
+  WiFiManager wifiManager;
+  //wifiManager.resetSettings();
+
+  if (!wifiManager.autoConnect("Foco - Red alternativa")) {
+    Serial.println("failed to connect and hit timeout");
+    // reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
   }
 
   Serial.println();
@@ -46,17 +44,6 @@ void setup_wifi(){
   Serial.println(WiFi.localIP());
 }
 
-void handleTelnet(){
-  if(server.hasClient()){
-    if(!telnet || !telnet.connected()){
-      if(telnet) telnet.stop();
-      telnet=server.available();
-    }
-    else{
-      server.available().stop();
-    }
-  }
-}
 
 void setup_mDNS(){
   if(mdns.begin("esp8266-01",WiFi.localIP())){
@@ -66,16 +53,16 @@ void setup_mDNS(){
 
 void setup_arduinoOTA(){
   ArduinoOTA.onStart([]() {
-   telnet.println("Start");
+  Serial.println("OTA: Start");
  });
  ArduinoOTA.onEnd([]() {
-   telnet.println("\nEnd");
+   Serial.println("\nEnd");
  });
  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-   telnet.printf("Progress: %u%%\r", (progress / (total / 100)));
+   Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
  });
  ArduinoOTA.onError([](ota_error_t error) {
-   telnet.printf("Error[%u]: ", error);
+   Serial.printf("Error[%u]: ", error);
    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
@@ -83,9 +70,6 @@ void setup_arduinoOTA(){
    else if (error == OTA_END_ERROR) Serial.println("End Failed");
  });
  ArduinoOTA.begin();
- telnet.println("Ready");
- telnet.print("IP address: ");
- telnet.println(WiFi.localIP());
 }
 
 void saveConfigCallback(){
@@ -137,37 +121,35 @@ void forceConfigMode(){
 }
 
 void callback(char* topic, byte* payload, unsigned int length){
-  telnet.println();
-  telnet.print("mensaje recibido [");
-  telnet.print(topic);
-  telnet.println("]");
+  Serial.println();
+  Serial.print("MQTT: mensaje recibido [");
+  Serial.print(topic);
+  Serial.println("]");
 
   char json[length];
   StaticJsonBuffer<40> jsonBuffer;
 
   for(int i=0;i<length;i++){
-    telnet.print((char)payload[i]);
+    Serial.print((char)payload[i]);
     json[i]=(char)payload[i];
   }
-  telnet.println();
+  Serial.println();
 
   JsonObject& root = jsonBuffer.parseObject(json);
   bool status=root["status"];
-  telnet.println(status);
+  Serial.println(status);
 
   StaticJsonBuffer<100> jsonBufferRoot;
   JsonObject &root2 = jsonBufferRoot.createObject();
 
   if(status){
-
     root2["status"] = true;
     String JSON;
     root2.printTo(JSON);
     const char *payload = JSON.c_str();
 
     estado_foco="Encendido";
-    digitalWrite(led,LOW);
-    telnet.println("prender");
+    digitalWrite(foco,LOW);
     Serial.println("prender");
     client.publish("foco_cambio", payload);
   }
@@ -179,8 +161,7 @@ void callback(char* topic, byte* payload, unsigned int length){
 
     estado_foco="Apagado";
 
-    digitalWrite(led, HIGH);
-    telnet.println("apagar");
+    digitalWrite(foco, HIGH);
     Serial.println("apagar");
     client.publish("foco_cambio", payload);
   }
@@ -189,18 +170,18 @@ void callback(char* topic, byte* payload, unsigned int length){
 
 void reconnect(){
   while(!client.connected()){
-    telnet.println("intentando conexion a mqtt...");
+    Serial.println("intentando conexion a mqtt...");
     String clientId="focoESP8266";
     if(client.connect(clientId.c_str(),"xbvmyoxh","nJzjyl5r-7GD")){
-      telnet.println("conectado");
+      //telnet.println("conectado");
       Serial.println("conectado");
       client.publish("foco_estado_conexion","esp8266 conectado");
       client.subscribe("encenderFoco");
     }
     else{
-      telnet.print("conexion fallida, rc= ");
-      telnet.print(client.state());
-      telnet.println("inteta de nuevo en 5 segundos");
+      Serial.print("conexion fallida, rc= ");
+      Serial.print(client.state());
+      Serial.println("inteta de nuevo en 5 segundos");
       delay(5000);
     }
   }
@@ -220,20 +201,18 @@ void setup(){
     Serial.println("se montó la FS");
   }
 
-  pinMode(led,OUTPUT);
-  digitalWrite(led, HIGH);
-  pinMode(sensor,INPUT);
-  pinMode(button,INPUT);
+  pinMode(foco,OUTPUT);
+  digitalWrite(foco, HIGH);
   loadConfig();
 
-  /*WiFiManager wifiManager;
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  //WiFiManager wifiManager;
+  /*wifiManager.setSaveConfigCallback(saveConfigCallback);
 
   WiFiManagerParameter customMqttServer("mqtt_server","mqtt server",mqtt_server,15);
   wifiManager.addParameter(&customMqttServer);
 
   wifiManager.autoConnect("esp-SEMARD","0008151964");
-  strcpy(mqtt_server,customMqttServer.getValue());*/
+  strcpy(mqtt_server,customMqttServer.getValue());/**/
 
   if(shouldSaveConfig){
     saveConfig();
@@ -250,13 +229,11 @@ void setup(){
 }
 
 void loop(){
-  //Serial.println(flashCorrectlyConfigured);
   Serial.print("-");
   delay(500);
   cronometro++;
   if(cronometro==10){
     cronometro=0;
-
     StaticJsonBuffer<100> jsonBufferRoot;
     JsonObject &root = jsonBufferRoot.createObject();
     if(estado_foco=="Encendido"){
@@ -270,24 +247,9 @@ void loop(){
     const char *payload = JSON.c_str();
     client.publish("foco_estado", payload);
   }
-  /*if(digitalRead(button)==LOW){
-    Serial.println("force config mode");
-    //forceConfigMode();
-  }*/
-  handleTelnet();
   ArduinoOTA.handle();
   if(!client.connected()){
     reconnect();
   }
   client.loop();
-
-  /*if(digitalRead(sensor)==LOW){
-    telnet.println("HIGH");
-    delay(500);
-  }
-  else{
-    telnet.println("LOW");
-    delay(500);
-  }*/
-
 }
